2d225993e2d87574be6059d0512dddeb4b64a57b
joelh <joelh@piquan.org>
2016-02-11 23:44:44 -0500

    Add upstart config files

--- etc/setup_host.sh
+++ etc/setup_host.sh
@@ -2,9 +2,19 @@
 
 set -ex
 
-# Turn this on if you want to actually install files into /etc (as
-# symlinks).  Otherwise, it will just build them.
-install=
+# Turn these on if you want to actually install files into /etc (as
+# symlinks where possible).  Otherwise, it will just build them.
+# Possible values are systemd, nginx, tmpfiles, and upstart.
+# Note: systemd and upstart should not be installed simultaneously.
+# Debian >= 7.0 "Wheezy" and Ubuntu >= 15.04 "Vivid Vervet" use
+# systemd, while earlier versions use upstart.
+#
+# Recent Raspbian / Debian / Ubuntu:
+#install="systemd nginx tmpfiles"
+# Slightly earlier Raspbian / Debian / Ubuntu:
+install="upstart nginx"
+# Manual:
+#install=""
 
 if [ ! -f ../config/default.json ] ; then
     echo You need to create or symlink ../config/default.json from one 2>&1
@@ -20,39 +30,33 @@ run_handlebars() {
 }
 
 run_handlebars nginx_site.conf
-if [ -n "$install" ] ; then
-    if [ -d /etc/nginx/sites-enabled/ ] ; then
-        sudo ln -sf $( pwd )/nginx_site.conf /etc/nginx/sites-enabled/a2-f1.conf
-    else
-        echo This script expects you to have nginx installed, and           >&2
-        echo configured with /etc/nginx/sites-enabled/ .  I went ahead and  >&2
-        echo build nginx_site.conf for you, but installing it is up to you. >&2
-        echo                                                                >&2
-    fi
-fi
+case "$install" in *nginx*)
+    sudo ln -sf $( pwd )/nginx_site.conf \
+        /etc/nginx/sites-enabled/a2-f1.conf
+    ;;
+esac
 
 run_handlebars a2-f1.target
 run_handlebars a2-f1-camdaemon.service
 run_handlebars a2-f1-cpanel.service
-if [ -n "$install" ] ; then
-    if [ -d /etc/systemd/ ] ; then
-        sudo systemctl enable $(pwd)/a2-f1.target $(pwd)/a2-f1-camdaemon.service $(pwd)/a2-f1-cpanel.service
-    else
-        echo This script expects you to be using systemd, but it looks like >&2
-        echo you have a different init.  You can look at the \*.service     >&2
-        echo files to get an idea of what you need to run.                  >&2
-        echo                                                                >&2
-    fi
-fi
+case "$install" in *systemd*)
+    sudo systemctl enable $(pwd)/a2-f1.target \
+        $(pwd)/a2-f1-camdaemon.service $(pwd)/a2-f1-cpanel.service
+    ;;
+esac
+
+run_handlebars a2-f1.upstart
+run_handlebars a2-f1-camdaemon.upstart
+run_handlebars a2-f1-cpanel.upstart
+case "$install" in *upstart*)
+    sudo cp a2-f1.upstart /etc/init/a2-f1.conf
+    sudo cp a2-f1-camdaemon.upstart /etc/init/a2-f1-camdaemon.conf
+    sudo cp a2-f1-cpanel.upstart /etc/init/a2-f1-cpanel.conf
+    ;;
+esac
 
 run_handlebars tmpfiles.conf
-if [ -n "$install" ] ; then
-    if [ -d /etc/tmpfiles.d/ ] ; then
-        sudo ln -sf $(pwd)/tmpfiles.conf /etc/tmpfiles.d/a2-f1.conf
-    else
-        echo This script expects you to be using tmpfiles.d, but it looks   >&2
-        echo like you do not have it installed.  You can look at            >&2
-        echo tmpfiles.conf to get an idea of what directories are needed.   >&2
-        echo                                                                >&2
-    fi
-fi
+case "$install" in *tmpfiles*)
+    sudo ln -sf $(pwd)/tmpfiles.conf /etc/tmpfiles.d/a2-f1.conf
+    ;;
+esac
